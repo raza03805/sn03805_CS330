fdce8c3434536a32b8e14f78b38e99956d96ef16
raza03805 <31271912+raza03805@users.noreply.github.com>
2019-02-13 18:37:34 +0500

    Add files via upload

---
+++ Lab02/Lab2b/top.v
@@ -0,0 +1,26 @@
+module top
+(
+	input clk, reset, rx,
+	output tx, [3:0] q
+);
+
+	wire [7:0] w;
+	assign w = {4'b0000, q};
+	ripple_carry_counter rcc0
+	(
+		.clk(clk),
+		.reset(reset),
+		.q(q)
+	);
+	
+	uart_top ut0
+	(
+		.clk(clk),
+		.reset(reset),
+		.rx(rx),
+		.tx(tx),
+		.data_to_tx(w)
+	);
+	
+
+endmodule
